11f9969e88634d28c453bc6719aaebaa11b9baaf
ngh1105 <ngh31502@gmail.com>
2026-05-19 15:57:59 +0700

    feat(phase3b): scaffold genlayer module with errors, types, mock

---
+++ src/lib/genlayer/index.ts
@@ -0,0 +1,5 @@
+export type { DecisionReceipt, CreateDecisionReceiptInput, ReceiptStatus, SubmitterKind } from "./types";
+export { GenLayerError, isGenLayerError, HTTP_STATUS_BY_CODE } from "./errors";
+export type { GenLayerErrorCode } from "./errors";
+export { getGenLayerService } from "./service";
+export type { GenLayerService } from "./service";
